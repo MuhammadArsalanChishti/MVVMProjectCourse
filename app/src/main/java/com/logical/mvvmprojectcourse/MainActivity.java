3e7f2363dbb9c19bf1768a517055371c36067472
DESKTOP-855DOIP\Arsalan <arsalanchishti4@gmail.com>
2019-10-04 00:53:10 +0500

    Merge branch 'master' of https://github.com/MuhammadArsalanChishti/MVVMProjectCourse into HomeBranch2
    # Conflicts:
    #	.idea/codeStyles/Project.xml
    #	.idea/misc.xml
    #	app/src/main/java/com/logical/mvvmprojectcourse/MainActivity.java
    
    merg finsh

--- app/src/main/java/com/logical/mvvmprojectcourse/MainActivity.java
+++ app/src/main/java/com/logical/mvvmprojectcourse/MainActivity.java
@@ -2,28 +2,21 @@ package com.logical.mvvmprojectcourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
-
-public class MainActivity extends AppCompatActivity {
-
-=======
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
 
-    //MVVM OfficeBranch
->>>>>>> d74edb99da02df36384c8a9e3fd4efc51c22ba81
+    //MVVM HomeBranch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-<<<<<<< HEAD
-=======
+
 
     private void toastMsg(String toast){
         Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
     }
->>>>>>> d74edb99da02df36384c8a9e3fd4efc51c22ba81
 }
